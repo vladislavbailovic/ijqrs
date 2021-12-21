b70e3e217cc3f3aacde7a6bf0cfb1a9a993a07be
vladislav <ve@incsub.com>
2021-12-21 05:58:00 +0100

    Write stdin to a temp file

--- src/app.rs
+++ src/app.rs
@@ -30,9 +30,10 @@ impl State {
     pub fn from_stdin() -> State {
         let mut source = String::from("");
         for line in io::stdin().lock().lines() {
-            source += String::from(line.expect("IO Error")).as_str();
+            source += (String::from(line.expect("IO Error")) + "\n").as_str();
         }
-        State::new("STDIN", &source)
+        let filename = write_temp(&source);
+        State::new(&filename, &source)
     }
 
     fn new(filename: &str, source: &str) -> State {
@@ -108,7 +109,6 @@ impl State {
 }
 
 fn run_command(command: &str, filename: &str) -> String {
-    // TODO: allow for STDIN
     let command = Command::new("jq")
         .arg(command)
         .arg(filename)
@@ -119,4 +119,21 @@ fn run_command(command: &str, filename: &str) -> String {
         return String::from_utf8(command.stderr).expect("Invalid stderr");
     }
     result
+}
+
+use std::env;
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::fs::File;
+use std::io::Write;
+
+fn write_temp(source: &str) -> String {
+    let mut tmp = env::temp_dir();
+    let now = SystemTime::now().duration_since(UNIX_EPOCH).expect("Error figuring out current time");
+    let fname = format!("ijqrs-{}.json", now.as_nanos());
+    tmp.push(fname);
+    let path = tmp.to_str().expect("Error getting temporary file path");
+
+    let mut file = File::create(path).expect("Error creating temp file");
+    file.write_all(source.as_bytes());
+    return String::from(path);
 }
